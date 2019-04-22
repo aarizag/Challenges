c334b301d41bdde9563355b86d5bfecf303c4c89
aarizag <adr.arizaga@gmail.com>
2019-04-21 17:52:41 -0700

    Completed unical tree problem

--- DailyProblem/Tree/unival_tree.py
+++ DailyProblem/Tree/unival_tree.py
@@ -17,4 +17,36 @@ class Node:
 
 
 def solution(head):
-    pass
+    solution.unival_trees = 0
+
+    def inner(node):
+        if node is None:
+            return None
+        l, r = inner(node.left), inner(node.right)
+        l = node.val if l is None else l
+        r = node.val if r is None else r
+
+        if l == node.val == r:
+            solution.unival_trees += 1
+        return node.val
+
+    inner(head)
+    return solution.unival_trees
+
+
+def test():
+    test_node = Node(1, left=Node(2,
+                                  left=Node(2), right=Node(2)),
+                        right=Node(3,
+                                   left=Node(4), right=Node(5))
+                     )
+    total = solution(test_node)
+
+    try:
+        assert total == 5
+        print("Test Passed")
+    except AssertionError:
+        print(f"Expected 5 subtrees, got {total}.")
+
+
+test()
